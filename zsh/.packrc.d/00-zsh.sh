9c4f7b490dab87845fea43febec88e9b3fffaccb
Arizona Hanson <arizona@starlig.ht>
2022-11-13 16:28:31 -0500

    zsh: better zshrc

--- zsh/.packrc.d/00-zsh.sh
+++ zsh/.packrc.d/00-zsh.sh
@@ -1,35 +1,34 @@
 # Only execute this file once per interactive shell.
 if [ -n "$__ZSH_SOURCED" ]; then return; fi
 __ZSH_SOURCED=1
-# Lines configured by zsh-newuser-install
+
+# history
 HISTFILE=~/.histfile
 HISTSIZE=9999
 SAVEHIST=8999
-bindkey -v
-# End of lines configured by zsh-newuser-install
-# The following lines were added by compinstall
-zstyle :compinstall filename "$HOME/.zshrc"
-
-autoload -Uz compinit
-compinit
-# End of lines added by compinstall
-# antigen
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS} ma='48;5;7;38;5;11'
-
-source "$HOME/.antigen.zsh"
-antigen use oh-my-zsh
-antigen bundle git
-antigen apply
-
 setopt INC_APPEND_HISTORY
 setopt HIST_EXPIRE_DUPS_FIRST
 setopt HIST_IGNORE_DUPS
 setopt HIST_IGNORE_SPACE
 setopt HIST_REDUCE_BLANKS
-# spellcheck commands
 setopt correct
-# turn off completion beeps
 unsetopt LIST_BEEP
+
+# compinstall
+zstyle :compinstall filename "$HOME/.zshrc"
+autoload -Uz compinit
+compinit
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS} ma='48;5;7;38;5;11'
+
+# antigen
+source "$HOME/.antigen.zsh"
+antigen use oh-my-zsh
+antigen bundle git
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
+antigen theme robbyrussell
+antigen apply
+
 # vi-like editing
 bindkey -v
 # backspace
@@ -54,7 +53,6 @@ bindkey -a "${terminfo[kend]}" vi-end-of-line
 bindkey '^w' vi-forward-word
 bindkey '^ ' autosuggest-accept
 # save prompt status
-ZLE_RPROMPT_INDENT=0
 zle-line-init() {
   typeset -g __prompt_status="$?"
 }
@@ -80,5 +78,7 @@ precmd() {
     echo -ne "\e[5 q"
   fi
 }
+
+ZLE_RPROMPT_INDENT=0
 
 source "$HOME/.zsh_aliases"
