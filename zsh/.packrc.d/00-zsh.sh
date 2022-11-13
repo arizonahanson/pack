3ab51f94e1246fc61e37f8555a51f4a225ce599b
Arizona Hanson <arizona@starlig.ht>
2022-11-13 14:41:17 -0500

    zsh: vi-like prompt improvements

--- zsh/.packrc.d/00-zsh.sh
+++ zsh/.packrc.d/00-zsh.sh
@@ -1,3 +1,6 @@
+# Only execute this file once per interactive shell.
+if [ -n "$__ZSH_SOURCED" ]; then return; fi
+__ZSH_SOURCED=1
 # Lines configured by zsh-newuser-install
 HISTFILE=~/.histfile
 HISTSIZE=9999
@@ -11,4 +14,59 @@ autoload -Uz compinit
 compinit
 # End of lines added by compinstall
 
+setopt INC_APPEND_HISTORY
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_REDUCE_BLANKS
+# vi-like editing
+bindkey -v
+# backspace
+bindkey -a '^?' vi-backward-delete-char
+# home
+bindkey -a '\e[1~' vi-first-non-blank
+bindkey '\e[1~' vi-first-non-blank
+# insert
+bindkey -a '\e[2~' vi-insert
+bindkey '\e[2~' vi-insert # noop?
+# delete
+bindkey '\e[3~' vi-delete-char
+bindkey -a '\e[3~' vi-delete-char
+# end
+bindkey -a '\e[4~'  vi-end-of-line
+bindkey '\e[4~'  vi-end-of-line
+bindkey  "${terminfo[khome]}" vi-beginning-of-line
+bindkey -a "${terminfo[khome]}" vi-beginning-of-line
+bindkey  "${terminfo[kend]}" vi-end-of-line
+bindkey -a "${terminfo[kend]}" vi-end-of-line
+# complete word
+bindkey '^w' vi-forward-word
+bindkey '^ ' autosuggest-accept
+# save prompt status
+ZLE_RPROMPT_INDENT=0
+zle-line-init() {
+  typeset -g __prompt_status="$?"
+}
+zle -N zle-line-init
+zle-keymap-select () {
+  if [ ! "$TERM" = "linux" ]; then
+    if [ $KEYMAP = vicmd ]; then
+      echo -ne "\e[1 q"
+    else
+      if [[ $ZLE_STATE == *insert* ]]; then
+        echo -ne "\e[5 q"
+      else
+        echo -ne "\e[3 q"
+      fi
+    fi
+  fi
+  () { return $__prompt_status }
+  zle reset-prompt
+}
+zle -N zle-keymap-select
+precmd() {
+  if [ ! "$TERM" = "linux" ]; then
+    echo -ne "\e[5 q"
+  fi
+}
 source "$HOME/.zsh_aliases"
