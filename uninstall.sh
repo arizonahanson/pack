dad3c7fcc8f01651d30befa8e4531ba6b2d79621
Arizona Hanson <arizona@starlig.ht>
2022-11-13 14:24:03 -0500

    vim: add vim pack

--- uninstall.sh
+++ uninstall.sh
@@ -2,5 +2,5 @@
 
 PACK_DIR="$(dirname "$(readlink -f "$0")")"
 
-"$PACK_DIR/pack/bin/pack" -D zsh pack
+"$PACK_DIR/pack/bin/pack" -D $(cat "$PACK_DIR/.packs")
 echo 'remove `source $HOME/.packrc.sh` from your $HOME/.zshrc'
