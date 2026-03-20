c8a75dbac50351d77c9de2539731761b3301d8af
Arizona Hanson <arizona@starlig.ht>
2026-03-20 13:12:58 -0400

    fix(bin): move `~/bin` to `~/.local/bin`

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 PACK_DIR="$(dirname $(readlink -f "$0"))"
 
-"$PACK_DIR/pack/bin/pack" pack zsh
+"$PACK_DIR/pack/.local/bin/pack" pack zsh
 echo 'add `source $HOME/.packrc.sh` to your $HOME/.zshrc'
